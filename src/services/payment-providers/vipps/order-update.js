2255d47cc7da528208245799c43be018d410d38e
Martin Andersen <marander@protonmail.com>
2021-10-18 14:55:26 +0200

    bb

--- src/services/payment-providers/vipps/order-update.js
+++ src/services/payment-providers/vipps/order-update.js
@@ -2,11 +2,11 @@ module.exports = async function vippsOrderUpdate({ crystallizeOrderId }) {
   console.log("VIPPS order update");
   console.log({ crystallizeOrderId });
 
-  // const { getClient } = require("./utils");
-  // const vippsClient = await getClient();
+  const { getClient } = require("./utils");
+   const vippsClient = await getClient();
 
   // Retrieve the Vipps order transaction details
-  // const order = await vippsClient.getOrderDetails({
-  //   orderId: crystallizeOrderId,
-  // });
+  const order = await vippsClient.getOrderDetails({
+     orderId: crystallizeOrderId,
+   });
 };
